f5487ee755da27de89cdabbbe0d9d7a117e101b1
Chris Trevino <darthtrevino@gmail.com>
2018-12-05 16:51:40 -0800

    break up the preloadWasm function

--- src/util/preloadWasm.ts
+++ src/util/preloadWasm.ts
@@ -1,6 +1,6 @@
 import { PyodideModule } from '../types'
 
-export function preloadWasm(pymod: Partial<PyodideModule>) {
+export function preloadWasm(pymod: PyodideModule) {
 	// On Chrome, we have to instantiate wasm asynchronously. Since that
 	// can't be done synchronously within the call to dlopen, we instantiate
 	// every .so that comes our way up front, caching it in the
@@ -8,33 +8,44 @@ export function preloadWasm(pymod: Partial<PyodideModule>) {
 	let promise = Promise.resolve()
 	const FS = window.pyodide._module.FS
 
-	function recurseDir(rootpath: string) {
-		let dirs
-		try {
-			dirs = FS.readdir(rootpath)
-		} catch {
-			return
-		}
-		for (const entry of dirs) {
-			if (entry.startsWith('.')) {
-				continue
-			}
-			const path = rootpath + entry
-			if (entry.endsWith('.so')) {
-				if (pymod.preloadedWasm[path] === undefined) {
-					promise = promise
-						.then(() => pymod.loadWebAssemblyModule!(FS.readFile(path), true))
-						.then(module => {
-							pymod.preloadedWasm[path] = module
-						})
-				}
-			} else if (FS.isDir(FS.lookupPath(path).node.mode)) {
-				recurseDir(path + '/')
-			}
+	const handleSoFile = (path: string) => {
+		if (pymod.preloadedWasm[path] === undefined) {
+			promise = promise
+				.then(() => pymod.loadWebAssemblyModule(FS.readFile(path), true))
+				.then(module => (pymod.preloadedWasm[path] = module))
 		}
 	}
 
-	recurseDir('/')
-
+	recurseDir('/', handleSoFile, FS)
 	return promise
+}
+
+/**
+ * Recurse a FS
+ * @param rootpath The root path to recurse
+ * @param handleSoFile Handler for .so files
+ * @param FS The FS Utility object
+ */
+function recurseDir(
+	rootpath: string,
+	handleSoFile: (path: string) => void,
+	FS: any,
+) {
+	let dirs
+	try {
+		dirs = FS.readdir(rootpath)
+	} catch {
+		return
+	}
+	for (const entry of dirs) {
+		if (entry.startsWith('.')) {
+			continue
+		}
+		const path = rootpath + entry
+		if (entry.endsWith('.so')) {
+			handleSoFile(path)
+		} else if (FS.isDir(FS.lookupPath(path).node.mode)) {
+			recurseDir(path + '/', handleSoFile, FS)
+		}
+	}
 }
